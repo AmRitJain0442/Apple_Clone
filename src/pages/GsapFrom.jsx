bb7f8a17b77af72638565b14108b7df114b92c85
Amrit Lahari <f20230442@pilani.bits-pilani.ac.in>
2025-05-20 22:02:51 +0530

    gsapfrom

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,13 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+  useGSAP(()=>{
+    gsap.from('#green-box',{
+      x:444,repeat:-1,duration:2,rotation:360,ease:'power1.inOut',yoyo:true
+    })
+  })
 
   return (
     <main>
